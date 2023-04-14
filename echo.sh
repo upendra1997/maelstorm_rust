14bcac1a6b650bceda9a9fe6c3c904cd91292109
Upendra Upadhyay <upendra.upadhyay@gojek.com>
2023-04-14 10:51:00 +0530

    using release version of echo

--- echo.sh
+++ echo.sh
@@ -1,2 +1,3 @@
 sdk use java 11.0.14.9.1-amzn
-../maelstrom/maelstrom test -w echo --bin ./target/debug/rustengan
+cargo build --release
+../maelstrom/maelstrom test -w echo --bin ./target/release/echo
